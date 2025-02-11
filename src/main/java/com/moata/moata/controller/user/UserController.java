79dda336b93e4f2baed548c888339c3c36f3da69
Hwangseoeun <sunny030921@gmail.com>
2025-02-11 21:18:29 +0900

    feat: 본인 프로필 조회 기능 구현

--- src/main/java/com/moata/moata/controller/user/UserController.java
+++ src/main/java/com/moata/moata/controller/user/UserController.java
@@ -1,10 +1,14 @@
 package com.moata.moata.controller.user;
 
+import com.moata.moata.config.jwt.TokenProvider;
 import com.moata.moata.dto.user.AuthTokens;
+import com.moata.moata.dto.user.UserProfileResponse;
 import com.moata.moata.service.user.KakaoService;
+import com.moata.moata.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,6 +22,8 @@ import java.util.NoSuchElementException;
 public class UserController {
 
     private final KakaoService kakaoService;
+    private final TokenProvider tokenProvider;
+    private final UserService userService;
 
     @ResponseBody
     @GetMapping("/auth/kakao/callback")
@@ -27,5 +33,14 @@ public class UserController {
         } catch (NoSuchElementException e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Item Not Found");
         }
+    }
+
+    @GetMapping("/user/my")
+    public ResponseEntity<UserProfileResponse> findUserProfile(Authentication authentication){
+
+        Long userId = Long.parseLong(authentication.getName());
+
+        UserProfileResponse response = userService.findUserProfileByUserId(userId);
+        return ResponseEntity.ok().body(response);
     }
 }
--- src/main/java/com/moata/moata/service/user/UserService.java
+++ src/main/java/com/moata/moata/service/user/UserService.java
@@ -1,7 +1,9 @@
 package com.moata.moata.service.user;
 
+import com.moata.moata.dto.user.UserProfileResponse;
 import com.moata.moata.entity.user.User;
 import com.moata.moata.repository.user.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,5 +16,11 @@ public class UserService {
     public User findById(Long userId) {
         return userRepository.findById(userId)
                 .orElseThrow(() -> new IllegalArgumentException("Unexpected user"));
+    }
+
+    public UserProfileResponse findUserProfileByUserId(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException("User not found"));
+        return UserProfileResponse.from(user);
     }
 }
